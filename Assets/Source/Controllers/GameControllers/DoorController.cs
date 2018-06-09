1536658ceb2aafec5a5be89056c3dab8f018e8d5
Vostek <alberto93.dev@gmail.com>
2018-06-09 11:16:44 +0200

    FEATURE: Adding the initial commit on level generation.

---
+++ Assets/Source/Controllers/GameControllers/DoorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    private LevelGenerator tile_manager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //tile_manager.ChangeTile()
+    }
+}
